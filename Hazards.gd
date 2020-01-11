856885f2b50c49100256e9929931b1fe92c87d5a
jayohanna <33985435+jayohanna@users.noreply.github.com>
2020-01-11 14:17:28 -0500

    moving laser v1

--- Hazards.gd
+++ Hazards.gd
@@ -19,13 +19,30 @@ func _ready():
 func spawn_laser():
 	var l = Laser.instance()
 	get_parent().add_child(l)
-	l.position.x = rand_range(0, 512)
-	l.position.y = rand_range(0, 300)
+	var side = randi()%4
+	match side:
+		0:
+			l.x_ini = rand_range(0, 512)
+			l.y_ini = 0
+		1:
+			l.x_ini = 512
+			l.y_ini = rand_range(0, 300)
+		2:
+			l.x_ini = rand_range(0, 512)
+			l.y_ini = 300
+		3:
+			l.x_ini = 0
+			l.y_ini = rand_range(0, 300)
+		_:
+			l.x_ini = 0
+			l.y_ini = 0
+	l.position.x = l.x_ini
+	l.position.y = l.y_ini
+	l.c = sqrt(pow(l.x_ini-l.x_towards, 2) + pow(l.y_ini - l.y_towards, 2))
+	l.x_vel = (l.x_towards - l.x_ini)/(l.c/l.velocity)
+	l.y_vel = (l.y_towards - l.y_ini)/(l.c/l.velocity)
+	l.ready = true
 	
 func _on_timer_timeout():
 	spawn_laser()
 	timer.start(6)
-
-func _physics_process(delta):
-	
-	pass
