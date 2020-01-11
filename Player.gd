62bd387d353b83d171156df83514d08f893a66c0
CaiEddie <>
2020-01-11 13:07:12 -0500

    added bullets

--- Player.gd
+++ Player.gd
@@ -1,6 +1,7 @@
 extends KinematicBody2D
 
 var Bees = preload("res://Bees.tscn")
+var Bullet = preload("res://Bullet.tscn")
 
 var timer = Timer.new()
 
@@ -12,6 +13,8 @@ var RIGHT
 var LEFT
 var DOWN
 var floor_timer = 0;
+var dashing = false;
+var dash_timer = 0;
 
 func _ready():
 	
@@ -38,6 +41,21 @@ func spawn_bee():
 	b.player_number = player_number
 	get_parent().add_child(b)
 
+func spawn_bullet():
+    # "Muzzle" is a Position2D placed at the barrel of the gun.
+	var a = Bullet.instance()
+	a.global_position = global_position
+	a.direction ="left"
+	a.player_number = player_number
+	get_parent().add_child(a)
+	
+	var b = Bullet.instance()
+	b.global_position = global_position
+	b.direction = "right"
+	b.player_number = player_number
+	get_parent().add_child(b)
+
+
 func _physics_process(delta):
 	
 	motion.y += 10
@@ -51,7 +69,7 @@ func _physics_process(delta):
 		
 		if Input.is_action_pressed(UP):
 			motion.y = -230
-			#get_node("AnimationPlayer").play("jump")
+			get_node("AnimationPlayer").play("jump")
 		
 		if Input.is_action_pressed(RIGHT):
 			motion.x = 120
@@ -59,6 +77,12 @@ func _physics_process(delta):
 			motion.x = -120
 		else:
 			motion.x = 0
+			
+		if dashing == true:
+			spawn_bullet()
+			dashing = false
+			dash_timer = 0
+		
 	else:
 		if Input.is_action_pressed(UP):
 			motion.y = clamp(motion.y, -250, 250)
@@ -74,13 +98,24 @@ func _physics_process(delta):
 		else:
 			motion.x = motion.x * 0.95
 		
+		if Input.is_action_pressed(DOWN):
+			dashing = true
+		
+		if dashing == true:
+			motion.x =0
+			motion.y =0
+			dash_timer += delta
+			if dash_timer > 0.2:
+				motion.x = 0
+				motion.y = 350
+				
+
+
 		motion.x = clamp(motion.x, -120, 120)
 		
 		
 	motion = move_and_slide(motion, Vector2(0, -1))
 	
-	
-
 func _on_timer_timeout():
 	spawn_bee()
 	timer.start(0.05)
