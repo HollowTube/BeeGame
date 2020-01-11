958341917111d087f22df816e4db60f8e7d2565f
CaiEddie <>
2020-01-10 21:13:50 -0500

    changed size of screen

--- Player.gd
+++ Player.gd
@@ -5,21 +5,21 @@ var motion = Vector2()
 
 func _physics_process(delta):
 	
-	motion.y += 4
+	motion.y += 8
 	
 	if Input.is_action_pressed("ui_right"):
-		motion.x = 100
+		motion.x = 120
 	elif Input.is_action_pressed("ui_left"):
-		motion.x = -100
+		motion.x = -120
 	else:
 		motion.x = 0
 		
 	if is_on_floor():
 		
 		if Input.is_action_pressed("ui_up"):
-			motion.y = -100
+			motion.y = -200
 		
 		
-	move_and_slide(motion, UP)
+	motion = move_and_slide(motion, UP)
 	
 	pass
