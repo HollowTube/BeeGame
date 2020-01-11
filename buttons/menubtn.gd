37af60b5ae94b54dcf03340f5ec1ad36da7ccf49
Ai Di Wang <aidiwang77@gmail.com>
2020-01-11 12:48:00 -0500

    changed buttons

--- buttons/menubtn.gd
+++ buttons/menubtn.gd
@@ -1,7 +1,6 @@
-extends Button
+extends TextureButton
 
 export(String) var scene_to_load
 
 func _on_startbtn_pressed():
 	pass # Replace with function body.
-
--- menu/buttons/menubtn2.gd
+++ menu/buttons/menubtn2.gd
@@ -1,7 +1,10 @@
-extends Button
+extends TextureButton
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
 
 export(String) var scene_to_load
 
 func _on_startbtn_pressed():
 	pass # Replace with function body.
-
