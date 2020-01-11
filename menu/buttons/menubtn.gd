03db0a68e7b05f672c44727c82113d7044f39f87
Ai Di Wang <aidiwang77@gmail.com>
2020-01-11 12:27:59 -0500

    changed menu

--- menu/buttons/menubtn.gd
+++ menu/buttons/menubtn.gd
@@ -1,3 +1,6 @@
 extends Button
 
 export(String) var scene_to_load
+
+func _on_startbtn_pressed():
+	pass # Replace with function body.
---
+++ menu/title_screen.gd
@@ -0,0 +1,20 @@
+extends Control
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	for button in $menu/center_row/buttons.get_children():
+		button.connect("pressed", self, "_on_button_pressed", [button.scene_to_load])
+#		button.connect("mouse_entered", self, "_on_mouse_entered")
+#		button.connect("mouse_exited", self, "_on_mouse_exited")
+		
+		
+
+func _on_button_pressed(scene_to_load):
+	get_tree().change_scene(scene_to_load)
+	
+# func _on_mouse_entered():
+	
