c1c158dfdfde081addf071f8a3661c5d54086a1f
CaiEddie <>
2020-01-10 21:26:37 -0500

    updated player movement

--- Player.gd
+++ Player.gd
@@ -5,20 +5,23 @@ var motion = Vector2()
 
 func _physics_process(delta):
 	
-	motion.y += 8
-	
-	if Input.is_action_pressed("ui_right"):
-		motion.x = 120
-	elif Input.is_action_pressed("ui_left"):
-		motion.x = -120
-	else:
-		motion.x = 0
-		
 	if is_on_floor():
 		
 		if Input.is_action_pressed("ui_up"):
-			motion.y = -200
+			motion.y = -250
 		
+		if Input.is_action_pressed("ui_right"):
+			motion.x = 120
+		elif Input.is_action_pressed("ui_left"):
+			motion.x = -120
+		else:
+			motion.x = 0
+	else:
+		if Input.is_action_pressed("ui_up"):
+			motion.y = clamp(motion.y + 10, -250, 250)
+		else:
+			motion.y = clamp(motion.y + 10, -100, 250)
+	
 		
 	motion = move_and_slide(motion, UP)
 	
