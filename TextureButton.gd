af65df0f1641e9e101eef0f96bb280255f6f05c0
CaiEddie <>
2020-01-12 13:06:52 -0500

    added ameover

---
+++ TextureButton.gd
@@ -0,0 +1,12 @@
+extends TextureButton
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+export(String) var scene_to_load
+
+func _on_startbtn_pressed():
+	pass # Replace with function body.
+	
+
--- end_text.gd
+++ end_text.gd
@@ -8,6 +8,14 @@ func _ready():
 	print(rand_line)
 	$text.text = rand_line
 	
+	for button in get_children():
+		if "TextureButton" in button.get_name():
+			if (button.get_name() != "quitbtn"):
+				button.connect("pressed", self, "_on_button_pressed", [button.scene_to_load])
+			else:
+				button.connect("pressed", self, "_on_quit_pressed")
+
+	
 func load_file(file_path):
 	var file = File.new()
 	file.open(file_path, file.READ)
@@ -24,9 +32,13 @@ func get_line_from_file(file_path):
 #func display_text(text):
 
 	
+func _on_button_pressed(scene_to_load):
+	
+	get_tree().paused= false
+	get_tree().change_scene(scene_to_load)
 
-
-
-
+	
+func _on_quit_pressed():
+	get_tree().quit()
 
 
