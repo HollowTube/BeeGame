a377309515553ab41e665c1931a883710349cd01
CaiEddie <>
2020-01-12 12:19:30 -0500

    changed name of world

--- vn.gd
+++ vn.gd
@@ -14,18 +14,18 @@ func _ready():
 	
 func _input(event):
 	if event.is_action_pressed("ui_accept"):
-		if (self.get_name() == "vn1"):
+		if (get_name() == "vn1"):
 			var b = load("res://vn1part2.tscn").instance()
 			get_parent().get_parent().add_child(b)
 			get_parent().queue_free()
-		elif (self.get_name() == "vn1part2"):
+		elif (get_name() == "vn1part2"):
 			var b = load("res://hazards.tscn").instance()
 			get_parent().get_parent().get_parent().get_node("World/Player").start = true
 			get_parent().get_parent().get_parent().get_node("World/Player2").start = true
 			get_parent().get_parent().get_parent().get_node("World").add_child(b)
 			get_parent().queue_free()
 			
-		elif (self.get_name() == "vn2"):
+		elif (get_name() == "vn2"):
 			get_tree().paused= false
 			get_tree().reload_current_scene()
 		
