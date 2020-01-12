fca27f8b973574a7677a7f5bab418e89b1e0beec
CaiEddie <>
2020-01-11 20:34:12 -0500

    added bird movement

--- Bees.gd
+++ Bees.gd
@@ -3,10 +3,12 @@ extends Node2D
 var armed = false
 var player_number = 0
 var fading = false
+var timer = Timer.new()
+var randomnumber = randi()	
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var timer = Timer.new()
+
 	timer.connect("timeout",self,"_on_timer_timeout") 
 	#timeout is what says in docs, in signals
 	#self is who respond to the callback
@@ -22,8 +24,12 @@ func _ready():
 #	pass
 
 func _physics_process(delta):
-	global_position.x = global_position.x + randi()%2-0.5
-	global_position.y = global_position.y + randi()%2-0.5
+	
+	if player_number == 1:
+		global_position.x = global_position.x + randi()%2-0.5
+		global_position.y = global_position.y + randi()%2-0.5
+	if player_number == 2:
+		global_position.y = global_position.y + 0.5*cos(timer.get_time_left()*5 + randomnumber)
 	
 	if fading == true:
 		$Sprite.modulate.a -= 0.1
